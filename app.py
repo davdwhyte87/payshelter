d73e8600b3f0901ed9c75bff45385a2b019da484
David whyte <kingstonwhyte87@gmail.com>
2018-08-14 05:35:36 -0700

    make it better

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.config.from_pyfile('settings.py',silent=True)
 
 @app.route('/')
 def hello_world():
-    return 'Whats up nigga! Thats the shit'
+    return 'Sup sup!'
 #run app
 if __name__=='__main__':
     app.run()
