d1217ea1aede5a60d2aadac6e22b7bae7c061301
David whyte <kingstonwhyte87@gmail.com>
2018-08-14 10:06:53 -0700

    config dburi

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 DEBUG=True
 SECRET_KEY="jdksjdksjdksj"
-SQLALCHEMY_DATABASE_URI="postgresql://postgres:hataguy@localhost:5432/payshelter"
+SQLALCHEMY_DATABASE_URI="postgres://lbexpbvkluhzto:72a14e178415dbaf452370c4bd2c1e922ccb0d291a2168ed8c23d75b6bf3997a@ec2-54-235-160-57.compute-1.amazonaws.com:5432/d2ji4psn40clhl"
+# SQLALCHEMY_DATABASE_URI="postgresql://postgres:hataguy@localhost:5432/payshelter"
 UPLOAD_FOLDER="image"
 ALLOWED_EXTENSIONS=(['png','jpg','jpeg','gif'])
