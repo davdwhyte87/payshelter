bfc1394d070d77a203b0046629da97933fcaffd5
David whyte <kingstonwhyte87@gmail.com>
2018-08-14 03:35:56 -0700

    testup for heuroku

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app=Flask(__name__,instance_relative_config=True)
+app.config.from_object('config.settings')
+app.config.from_pyfile('settings.py',silent=True)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+#run app
+if __name__=='__main__':
+    app.run()
---
+++ config/settings.py
@@ -0,0 +1,5 @@
+DEBUG=True
+SECRET_KEY="jdksjdksjdksj"
+# SQLALCHEMY_DATABASE_URI="postgresql://postgres:hataguy@localhost:5432/haita"
+# UPLOAD_FOLDER="image"
+# ALLOWED_EXTENSIONS=(['png','jpg','jpeg','gif'])
